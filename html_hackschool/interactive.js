f84b7c13851df224554dc076bce225c93b281f8e
Isabelle Goralsky <isabellegoral@g.ucla.edu>
2020-12-07 13:43:08 -0800

    initial commit

---
+++ html_hackschool/interactive.js
@@ -0,0 +1,18 @@
+﻿const home = document.querySelector(".home");
+const change = ()=>{
+	window.location.href = "index.html";
+}
+home.addEventListener("click",change);
+
+const res = document.querySelector(".res");
+const change2 = ()=>{
+	window.location.href = "resume_isabelle-goralsky.pdf";
+}
+res.addEventListener("click",change2);
+
+
+const email = document.querySelector(".email");
+const change3 = ()=>{
+	window.location.href = "mailto: isabellegoral@g.ucla.edu";
+}
+email.addEventListener("click",change3);
